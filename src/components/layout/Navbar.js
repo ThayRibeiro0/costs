ef64db88016595132f053fb94d3305e91232ddd5
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-09-16 15:58:21 -0700

    3

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom'
 import Container from './Container'
 // import styles from './Navbar.module.css'
-import logo from '../../img/costs_logo.png'
+import logo from '../img/costs_logo.png'
 
 function Navbar(){
     return(
