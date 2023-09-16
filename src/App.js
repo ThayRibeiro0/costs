a1a23b0dab12364d07a9d91fda14b7763934a189
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-09-16 13:47:26 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+
+      <Routes>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        
+        <Route exact path="/company">
+          <Company/>
+        </Route>
+
+        <Route exact path="/contact">
+          <Contact/>
+        </Route>
+        
+        <Route exact path="/newproject">
+          <NewProject/>
+        </Route>
+      </Routes>
+
+      <p>Footer</p>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,5 @@
+function Contact(){
+    return <h1>Home</h1>
+}
+
+export default Contact
