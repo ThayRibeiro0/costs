cbed048e758b4e02b971fe9967649bc5ff661f6d
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-10-01 10:39:20 -0700

    up2

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,7 @@ function Home(){
       <section className={styles.home_container}>
         <h1>Welcome to <span>Costs</span></h1>
         <p>Start to manage your projects now</p>
-        <LinkButton to="/newproject" text="Create Projec t"/>
+        <LinkButton to="/newproject" text="Create Project"/>
        <img src={savings} alt="Costs"/>
       </section>
     )
