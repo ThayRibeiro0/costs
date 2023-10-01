432d14d00335ad361ccaee407f6ef104472f989b
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-10-01 13:56:42 -0700

    upd4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Projects from './components/pages/Projects'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
+import Project from './components/pages/Project'
 
 import Container from './components/layout/Container'
 import Navbar from './components/layout/Navbar'
@@ -17,10 +18,11 @@ function App() {
       <Container customClass="min-Height">
         <Routes>
           <Route exact path="/" element={<Home/>}/>
-          <Route exact path="/projects" element={<Projects/>}/>
-          <Route exact path="/company" element={<Company/>}/>
-          <Route exact path="/contact" element={<Contact/>}/>
-          <Route exact path="/newproject" element={<NewProject/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/company" element={<Company/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/newproject" element={<NewProject/>}/>
+          <Route path="/project/:id" element={<Project/>}/>
         </Routes>
       </Container>
 
---
+++ src/components/pages/Project.js
@@ -0,0 +1,7 @@
+import styles from './Project.module.css'
+
+function Project(){
+    return <p>Project</p>
+}
+
+export default Project
