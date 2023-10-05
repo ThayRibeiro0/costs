035261c27047d1d9489c52b08fe3ee3fea80a1b9
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-10-04 21:23:40 -0700

    vamos la

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
-import Projects from './components/pages/Projects'
+// import Projects from './components/pages/Projects'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
-// import NewProject from './components/pages/NewProject'
+import NewProject from './components/pages/NewProject'
 // import Project from './components/pages/Project'
 
 import Container from './components/layout/Container'
@@ -18,10 +18,10 @@ function App() {
       <Container customClass="min-Height">
         <Routes>
           <Route exact path="/" element={<Home/>}/>
-          <Route path="/projects" element={<Projects/>}/>
+          {/* <Route path="/projects" element={<Projects/>}/> */}
           <Route path="/company" element={<Company/>}/>
           <Route path="/contact" element={<Contact/>}/>
-          {/* <Route path="/newproject" element={<NewProject/>}/> */}
+          <Route path="/newproject" element={<NewProject/>}/>
           {/* <Route path="/project/:id" element={<Project/>}/> */}
         </Routes>
       </Container>
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,40 +1,40 @@
-// import { useNavigate } from 'react-router-dom'
-// import styles from './NewProject.module.css'
-// import ProjectForm from '../project/ProjectForm'
+import { useNavigate } from 'react-router-dom'
+import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm'
 
 
-// function NewProject(){
+function NewProject(){
 
-//     const navigate = useNavigate()
+    const navigate = useNavigate()
 
-//     function createPost(project){
+    function createPost(project){
         
-//         // initialize cost and services
-//         project.cost = 0
-//         project.services = []
+        // initialize cost and services
+        project.cost = 0
+        project.services = []
 
-//         fetch("http://localhost:8080/projects", {
-//             method: 'POST',
-//             headers: {
-//                 'Content-type': 'application/json',
-//            },
-//            body: JSON.stringify(project),
-//         }).then((resp) => resp.json())
-//         .then((data) => {
-//             console.log(data)
-//             //redirect
-//             navigate('/projects',{state: {message: 'Project created with sucess!'}})
-//         })
-//         .catch(err => console.log(err))
-//     }
+        fetch("http://localhost:8080/projects", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+           },
+           body: JSON.stringify(project),
+        }).then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            //redirect
+            navigate('/projects',{state: {message: 'Project created with sucess!'}})
+        })
+        .catch(err => console.log(err))
+    }
 
-//     return (
-//         <div className={styles.newproject_container}>
-//             <h1>Create Project</h1>
-//             <p>Create your project to after add your services</p>
-//             <ProjectForm handleSubmit={createPost} btnText="Create Project"/>
-//         </div>
-//     )
-// }
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Create Project</h1>
+            <p>Create your project to after add your services</p>
+            <ProjectForm handleSubmit={createPost} btnText="Create Project"/>
+        </div>
+    )
+}
 
-// export default NewProject
+export default NewProject
