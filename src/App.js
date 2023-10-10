da1cd8ee4196db596365590569f7992b032cb49f
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-10-09 22:07:37 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
       <Container customClass="min-Height">
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects/>}/>
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
